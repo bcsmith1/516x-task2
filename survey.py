57b3470ff080c70ba37b8c0b35512c10cbbb7553
bcsmith1 <54888622+bcsmith1@users.noreply.github.com>
2019-09-10 10:51:23 -0500

    Update survey.py

--- survey.py
+++ survey.py
@@ -2,4 +2,4 @@ import pandas as pd
 url = "https://raw.githubusercontent.com/isu-abe/516x/master/data/surveys1977.csv"
 survey = pd.read_csv(url, sep = ",", index_col=0)
 survey.head()
-survey.describe()
+print(survey.describe())
