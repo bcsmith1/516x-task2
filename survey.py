7b641c5d752a0eade27a72ae16f29a94e236c245
bcsmith1 <54888622+bcsmith1@users.noreply.github.com>
2019-09-10 11:18:45 -0500

    update

--- survey.py
+++ survey.py
@@ -1,5 +1,9 @@
 import pandas as pd
-url = "https://raw.githubusercontent.com/isu-abe/516x/master/data/surveys1977.csv"
-survey = pd.read_csv(url, sep = ",", index_col=0)
+import sys
+
+#url = "https://raw.githubusercontent.com/isu-abe/516x/master/data/surveys1977.csv"
+file_to_open = open(sys.argv[1], 'r')
+survey = pd.read_csv(file_to_open, sep = ",", index_col=0)
 survey.head()
 print(survey.describe())
+
